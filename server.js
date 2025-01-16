95d432bcc38f4d7e7def52381d75da589cdcc71b
monjane10 <louramonja52.n@gmail.com>
2025-01-16 18:38:24 +0200

    conexao com a base de dados e insercao de utilizadores

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-import express from 'express'
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
+app.post('/utilizadores', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
 
-app.post('/utilizadores', (req, res) => {
-    users.push(req.body);
-    console.log(req.body);
-    res.send('Utilizador criado');
-})
-
-app.get('/utilizadores', (req, res) => {
-  res.json(users);
+    // Adicionar o usuário na lista (se necessário)
+    res.status(201).send('Utilizador criado com sucesso!');
+  } catch (error) {
+    // Tratar o erro e enviar uma única resposta
+    console.error('Erro ao criar o utilizador:', error.message);
+    res.status(500).send('Erro ao criar o utilizador');
+  }
 });
 
-app.listen(3000, () => {  
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
 });
